e562264724b217a3685e1d9fb04bde762103a793
Eben <96250954+EbenJesusSaves@users.noreply.github.com>
2024-10-06 01:00:00 +0000

    new concepts

--- express/index.js
+++ express/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import path from 'node:path'
+
 
 const app = express()
 
+console.log(path)
 
 // this is express json | and I don't  know what I'm doing 
 app.use(express.json())
